7eadc7cb7d01285e74878d8ef250ba394090ee0e
hbertini-ds <61480956+hbertini-ds@users.noreply.github.com>
2020-06-11 15:56:55 +0100

    removed unnecessary bat file.

--- jupyterlab-here_ds-misc.bat
+++
@@ -1,1 +0,0 @@
-%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'C:\Users\hbertini\AppData\Local\Continuum\anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'DS Misc' ; jupyter lab
